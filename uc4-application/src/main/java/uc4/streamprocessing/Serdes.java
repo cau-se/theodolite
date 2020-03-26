6bc5224e3d7ece0ca170c8856642671f809e1642
Sören Henning <post@soeren-henning.de>
2020-03-26 15:03:28 +0100

    Remove currently unused SchemaRegistry-related things

--- uc4-application/src/main/java/uc4/streamprocessing/Serdes.java
+++ uc4-application/src/main/java/uc4/streamprocessing/Serdes.java
@@ -1,41 +1,34 @@
 package uc4.streamprocessing;
 
 import com.google.common.math.Stats;
-import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.common.serialization.Serde;
 import titan.ccp.common.kafka.GenericSerde;
-import titan.ccp.common.kafka.avro.SchemaRegistryAvroSerdeFactory;
-import titan.ccp.model.records.ActivePowerRecord;
-import titan.ccp.model.records.AggregatedActivePowerRecord;
-import titan.ccp.model.records.WindowedActivePowerRecord;
 
 final class Serdes {
 
-  private final SchemaRegistryAvroSerdeFactory avroSerdeFactory;
+  // private final SchemaRegistryAvroSerdeFactory avroSerdeFactory;
 
   public Serdes(final String schemaRegistryUrl) {
-    this.avroSerdeFactory = new SchemaRegistryAvroSerdeFactory(schemaRegistryUrl);
+    // this.avroSerdeFactory = new SchemaRegistryAvroSerdeFactory(schemaRegistryUrl);
   }
 
   public Serde<String> string() {
     return org.apache.kafka.common.serialization.Serdes.String();
   }
 
-  public Serde<WindowedActivePowerRecord> windowedActivePowerValues() {
-    return this.avroSerdeFactory.forKeys();
-  }
-
-  public Serde<ActivePowerRecord> activePowerRecordValues() {
-    return this.avroSerdeFactory.forValues();
-  }
-
-  public Serde<AggregatedActivePowerRecord> aggregatedActivePowerRecordValues() {
-    return this.avroSerdeFactory.forValues();
-  }
-
-  public <T extends SpecificRecord> Serde<T> avroValues() {
-    return this.avroSerdeFactory.forValues();
-  }
+  /*
+   * public Serde<WindowedActivePowerRecord> windowedActivePowerValues() { return
+   * this.avroSerdeFactory.forKeys(); }
+   * 
+   * public Serde<ActivePowerRecord> activePowerRecordValues() { return
+   * this.avroSerdeFactory.forValues(); }
+   * 
+   * public Serde<AggregatedActivePowerRecord> aggregatedActivePowerRecordValues() { return
+   * this.avroSerdeFactory.forValues(); }
+   * 
+   * public <T extends SpecificRecord> Serde<T> avroValues() { return
+   * this.avroSerdeFactory.forValues(); }
+   */
 
   public Serde<Stats> stats() {
     return GenericSerde.from(Stats::toByteArray, Stats::fromByteArray);
