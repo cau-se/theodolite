d3b5ea08bfb6bd555639b356e8c93749f0cc250a
Sören Henning <post@soeren-henning.de>
2020-05-11 11:24:42 +0200

    Add configuration for Kafka lag exporter

---
+++ execution/infrastructure/kafka-lag-exporter/install.sh
@@ -0,0 +1,6 @@
+helm install kafka-lag-exporter https://github.com/lightbend/kafka-lag-exporter/releases/download/v0.6.0/kafka-lag-exporter-0.6.0.tgz \
+  --set clusters\[0\].name=my-confluent-cp-kafka \
+  --set clusters\[0\].bootstrapBrokers=my-confluent-cp-kafka:9092 \
+  --set pollIntervalSeconds=15 #5
+
+# Helm could also create ServiceMonitor
