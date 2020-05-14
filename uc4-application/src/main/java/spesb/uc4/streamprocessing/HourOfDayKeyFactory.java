0a529341e07767be3af28cef6bdc3d845d8644ce
Björn Vonheiden <bjoern.vonheiden@hotmail.de>
2020-05-14 12:50:42 +0200

    Change top level java package to spesb and adopt main class in gradle
    Add the spesb prefix to all self implemented java packages of the
    project. Because this changes the fully qualified class name of
    the main classes the build.gradle files need to adopt to this.

---
+++ uc4-application/src/main/java/spesb/uc4/streamprocessing/HourOfDayKeyFactory.java
@@ -0,0 +1,21 @@
+package spesb.uc4.streamprocessing;
+
+import java.time.LocalDateTime;
+
+/**
+ * {@link StatsKeyFactory} for {@link HourOfDayKey}.
+ */
+public class HourOfDayKeyFactory implements StatsKeyFactory<HourOfDayKey> {
+
+  @Override
+  public HourOfDayKey createKey(final String sensorId, final LocalDateTime dateTime) {
+    final int hourOfDay = dateTime.getHour();
+    return new HourOfDayKey(hourOfDay, sensorId);
+  }
+
+  @Override
+  public String getSensorId(final HourOfDayKey key) {
+    return key.getSensorId();
+  }
+
+}
