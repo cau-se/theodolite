ea2aa9eefec92860971a500d0b31da61590b4bd2
Simon Ehrenstein <simon.ehrenstein@gmail.com>
2020-06-14 18:02:41 +0200

    Add Javadoc

--- workload-generator-commons/src/main/java/theodolite/commons/workloadgeneration/functions/Transport.java
+++ workload-generator-commons/src/main/java/theodolite/commons/workloadgeneration/functions/Transport.java
@@ -2,6 +2,12 @@ package theodolite.commons.workloadgeneration.functions;
 
 import kieker.common.record.IMonitoringRecord;
 
+/**
+ * This interface describes a function that consumes a {@link IMonitoringRecord}. This function is
+ * dedicated to be used to transport individual messages to the messaging system.
+ *
+ * @param <T> the type of records to send as messages.
+ */
 @FunctionalInterface
 public interface Transport<T extends IMonitoringRecord> {
 
