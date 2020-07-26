67423b5fb2a25840d346848251dbd818295695aa
Björn Vonheiden <bjoern.vonheiden@hotmail.de>
2020-07-26 15:50:41 +0200

    Abstract functional Transport interface from workload generator.
    Abstract the Interface to be more generic.

--- workload-generator-commons/src/main/java/theodolite/commons/workloadgeneration/functions/Transport.java
+++ workload-generator-commons/src/main/java/theodolite/commons/workloadgeneration/functions/Transport.java
@@ -1,15 +1,13 @@
 package theodolite.commons.workloadgeneration.functions;
 
-import kieker.common.record.IMonitoringRecord;
-
 /**
- * This interface describes a function that consumes a {@link IMonitoringRecord}. This function is
- * dedicated to be used to transport individual messages to the messaging system.
+ * This interface describes a function that consumes a message {@code T}. This function is dedicated
+ * to be used to transport individual messages to the messaging system.
  *
  * @param <T> the type of records to send as messages.
  */
 @FunctionalInterface
-public interface Transport<T extends IMonitoringRecord> {
+public interface Transport<T> {
 
   void transport(final T message);
 
