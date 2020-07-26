def468fe5f95812b7e08e536b099d6d42945010e
Simon Ehrenstein <simon.ehrenstein@gmail.com>
2020-07-26 23:00:53 +0200

    Use fomat strings

--- execution/strategies/strategies/default_strategy.py
+++ execution/strategies/strategies/default_strategy.py
@@ -9,7 +9,7 @@ def execute(config):
     for dim_value in config.dim_values:
         for replica in config.replicas:
             subexperiment_counter+=1
-            print("Run subexperiment " + str(subexperiment_counter) + "/" + str(subexperiments_total) + " with config " + str(dim_value) + " " + str(replica))
+            print("Run subexperiment {}/{} with dimension value {} and {} replicas.".format(subexperiment_counter, subexperiments_total, dim_value, replica))
 
             subexperiment_config = SubexperimentConfig(config.use_case, subexperiment_counter, dim_value, replica, config.partitions, config.cpu_limit, config.memory_limit, config.kafka_streams_commit_interval_ms, config.execution_minutes, config.subexperiment_executor)
 
