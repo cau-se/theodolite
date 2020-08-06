a5eb5aaa2a707c23c947f96772f39bde1f3f808f
Simon Ehrenstein <simon.ehrenstein@gmail.com>
2020-08-06 12:00:27 +0200

    Rename called script

--- execution/strategies/subexperiment_execution/subexperiment_executor.py
+++ execution/strategies/subexperiment_execution/subexperiment_executor.py
@@ -5,4 +5,4 @@ import os
 dirname = os.path.dirname(__file__)
 os.chdir(dirname+"/../../")
 def execute(subexperiment_config):
-    os.system("./run_uc{}-new.sh {} {} {} {} {} {} {} {}".format(subexperiment_config.use_case, subexperiment_config.counter, subexperiment_config.dim_value, subexperiment_config.replicas, subexperiment_config.partitions, subexperiment_config.cpu_limit, subexperiment_config.memory_limit, subexperiment_config.kafka_streams_commit_interval_ms, subexperiment_config.execution_minutes))
+    os.system("./run_uc{}.sh {} {} {} {} {} {} {} {}".format(subexperiment_config.use_case, subexperiment_config.counter, subexperiment_config.dim_value, subexperiment_config.replicas, subexperiment_config.partitions, subexperiment_config.cpu_limit, subexperiment_config.memory_limit, subexperiment_config.kafka_streams_commit_interval_ms, subexperiment_config.execution_minutes))
