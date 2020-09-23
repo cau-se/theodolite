52d9084595fc8305e0860b26bed8ce88da7d86d7
Simon Ehrenstein <simon.ehrenstein@gmail.com>
2020-09-23 12:26:16 +0200

    Use exp_id in evaluation

--- execution/strategies/subexperiment_evaluation/subexperiment_evaluator.py
+++ execution/strategies/subexperiment_evaluation/subexperiment_evaluator.py
@@ -8,7 +8,7 @@ WARMUP_SEC = 60
 
 def execute(config):
     cwd = os.getcwd()
-    file = f"exp{config.counter}_uc{config.use_case}_{config.dim_value}_{config.replicas}_totallag.csv"
+    file = f"exp{config.exp_id}_uc{config.use_case}_{config.dim_value}_{config.replicas}_totallag.csv"
 
     trend_slope = trend_slope_computer.compute(cwd, file, WARMUP_SEC, THRESHOLD)
 
