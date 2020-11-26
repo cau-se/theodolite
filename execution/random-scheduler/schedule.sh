5c099bba2364a2a726ba94ef0790a6e626ab5092
Simon Ehrenstein <simon.ehrenstein@gmail.com>
2020-11-26 19:18:44 +0100

    Add custom random-scheduler

---
+++ execution/random-scheduler/schedule.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SERVER=($(kubectl config view -o json | jq '.clusters[0].cluster.server'))
+
+while true;
+
+do
+
+    echo "$(kubectl get pods -n default)"
+    for PODNAME in $(kubectl get pods -n default -o json | jq '.items[] | select(.spec.schedulerName == "random-scheduler") | select(.spec.nodeName == null) | .metadata.name' | tr -d '"');
+
+    do
+
+        NODES=($(kubectl get nodes -o json | jq '.items[].metadata.name' | tr -d '"'))
+
+
+        NUMNODES=${#NODES[@]}
+
+        CHOSEN=${NODES[$[$RANDOM % $NUMNODES]]}
+
+        curl --header "Content-Type:application/json" --request POST --data '{"apiVersion":"v1", "kind": "Binding", "metadata": {"name": "'$PODNAME'"}, "target": {"apiVersion": "v1", "kind": "Node", "name": "'$CHOSEN'"}}' $SERVER/api/v1/namespaces/default/pods/$PODNAME/binding/
+
+        echo "Assigned $PODNAME to $CHOSEN"
+
+    done
+
+    sleep 1
+
+done
