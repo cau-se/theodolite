ba6fb220b8a2ef1d72ad01e50896bd3232343c42
lorenz <stu203404@mail.uni-kiel.de>
2020-12-17 01:25:44 +0100

    Add own main method

---
+++ theodolite-quarkus/src/main/kotlin/theodolite/Main.kt
@@ -0,0 +1,14 @@
+package theodolite
+import io.quarkus.runtime.annotations.QuarkusMain
+
+
+@QuarkusMain
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Running main method")
+
+
+        //Quarkus.run()
+    }
+}
