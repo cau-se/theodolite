e8b7fc6c708e1bce470479e27f7b8b68cdb32f6f
lorenz <stu203404@mail.uni-kiel.de>
2021-01-10 09:36:09 +0100

    Start with skelet from run_uc.py

---
+++ theodolite-quarkus/src/main/kotlin/theodolite/Run_uc.kt
@@ -0,0 +1,28 @@
+package theodolite
+
+class Run_uc {
+
+
+ fun wait_execution(execution_minutes :Integer){
+
+
+     return
+    }
+
+
+    fun create_topics(topics: List<Pair<String,String>>){
+
+    }
+
+    fun delete_topics(topics: List<Pair<String,String>>){
+
+    }
+
+    fun reset_zookeeper(){
+
+    }
+
+    fun start_workload_generator(wg: String, dim_value:Integer, uc_id: String){
+        
+    }
+}
