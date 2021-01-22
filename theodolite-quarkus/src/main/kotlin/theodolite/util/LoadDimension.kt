0fafc90d87470ec870262adde846e8796286ae8b
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-01-22 11:46:45 +0100

    use data classes

--- theodolite-quarkus/src/main/kotlin/theodolite/util/LoadDimension.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/util/LoadDimension.kt
@@ -1,17 +1,7 @@
 package theodolite.util
 
-class LoadDimension(val number: Int) { // data class ?
+data class LoadDimension(val number: Int) {
     public fun get(): Int {
         return this.number;
-    }
-    public override fun equals(other: Any?): Boolean {
-        if (other is LoadDimension) {
-            return this.get() == other.get()
-        }
-        return false
-    }
-
-    override fun hashCode(): Int {
-        return this.get().hashCode()
     }
 }
--- theodolite-quarkus/src/main/kotlin/theodolite/util/Resource.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/util/Resource.kt
@@ -1,18 +1,7 @@
 package theodolite.util
 
-class Resource(val number: Int) {
+data class Resource(val number: Int) {
     public fun get(): Int {
         return this.number;
-    }
-
-    public override fun equals(other: Any?): Boolean {
-        if (other is Resource) {
-            return this.get() == other.get()
-        }
-        return false
-    }
-
-    override fun hashCode(): Int {
-        return this.get().hashCode()
     }
 }
