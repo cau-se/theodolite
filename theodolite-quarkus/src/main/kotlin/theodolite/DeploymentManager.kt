563a65ec9b45b042ba6b554ad55ab344cd6d6e45
lorenz <stu203404@mail.uni-kiel.de>
2021-01-23 03:21:34 +0100

    Add limit and Image exchange funtions

--- theodolite-quarkus/src/main/kotlin/theodolite/DeploymentManager.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/DeploymentManager.kt
@@ -1,6 +1,7 @@
 package theodolite
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.fabric8.kubernetes.api.model.Quantity
 import io.fabric8.kubernetes.api.model.Service
 import io.fabric8.kubernetes.api.model.apps.Deployment
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
@@ -11,12 +12,15 @@ import java.nio.file.Paths
 
 
 class DeploymentManager {
+    val MEMORYLIMIT = "memory"
+    val CPULIMIT = "memory"
 
     val absolute = Paths.get("").toAbsolutePath().toString()
     val path = "/home/lorenz/git/spesb/theodolite-quarkus/YAML/"
     val theodoliteDeploment = "theodolite.yaml"
     val service = "aggregation-service.yaml"
     val workloadFile = "workloadGenerator.yaml"
+    val usecase = "aggregation-deployment.yaml"
     val inputStream: InputStream = path.byteInputStream()
     val client = DefaultKubernetesClient().inNamespace("default")
 
@@ -25,25 +29,32 @@ class DeploymentManager {
     val dp: Service = client.services().load(path+service).get();
 
     val workload : Deployment = client.apps().deployments().load(path+workloadFile).get();
+    val use : Deployment = client.apps().deployments().load(path+usecase).get();
+
 
     // TODO MAKE YAML LOADING CATCH EXEPTION
 
 
     fun printFile(){
 
+//
+//        println(workload)
+//        changeWorkloadNumInstances(workload,"5000")
+//        println(workload)
 
-        //println(workload)
-        changeWorkloadNumInstances(workload,5000)
-        //println(workload)
+        println(use)
+        changeRessourceLimits(use, MEMORYLIMIT,"5Gi")
+        println(use)
 
 
-        println(path)
-        val f : File = File(path+theodoliteDeploment);
-        val fileAsString : String = String(f.readBytes())
-        //println(fileAsString.replace("theodolite","spesb"))
+//        println(path)
+//        val f : File = File(path+theodoliteDeploment);
+//        val fileAsString : String = String(f.readBytes())
+//        println(fileAsString.replace("theodolite","spesb"))
     }
 
 
+    // SERVICE
     fun changeServiceName (service: Service,newName : String){
 
         service.metadata.apply {
@@ -51,19 +62,28 @@ class DeploymentManager {
         }
     }
 
-    fun changeWorkloadNumInstances (dep: Deployment,num: String){
+    // WORKLOAD GEN
+    fun changeWorkloadNumInstances (dep: Deployment,num: String) {
 
-        val vars = dep.spec.template.spec.containers.get(0).env.filter {
+        dep.spec.template.spec.containers.get(0).env.filter {
             it.name == "NUM_SENSORS"
-        }.forEach {
-            x ->
-                    x.value = num
+        }.forEach { x ->
+            x.value = num
         }
-
-
-        println(vars)
-
     }
 
+    // APPLICATION
+    fun changeRessourceLimits(dep: Deployment, ressource: String, limit: String) {
+
+        val vars = dep.spec.template.spec.containers.filter { it.name == "uc-application" }.forEach {
+            it.resources.limits.replace(ressource, Quantity(limit))
+        }
+    }
+
+    fun changeImage(dep: Deployment, image: String) {
+
+        dep.spec.template.spec.containers.filter { it.name == "uc-application" }.forEach {
+            it.image = image }
+    }
 
 }
