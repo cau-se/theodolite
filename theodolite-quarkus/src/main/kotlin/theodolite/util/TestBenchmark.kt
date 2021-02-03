4d3587f52db95098967daede31f0406642380673
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-02-03 14:56:06 +0100

    update test to work with new interfaces

--- theodolite-quarkus/src/main/kotlin/theodolite/util/TestBenchmark.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/util/TestBenchmark.kt
@@ -1,7 +1,22 @@
 package theodolite.util
 
-class TestBenchmark: Benchmark(config = emptyMap()) {
-    override fun start() {
+import theodolite.k8s.UC1Benchmark
+
+class TestBenchmark: Benchmark(UC1Benchmark.UC1BenchmarkConfig(
+    zookeeperConnectionString = "",
+    kafkaIPConnectionString = "",
+    schemaRegistryConnectionString = "",
+    kafkaTopics = emptyList(),
+    kafkaReplication = 0,
+    kafkaPartition  = 0,
+    ucServicePath = "",
+    ucDeploymentPath = "",
+    wgDeploymentPath = "",
+    ucImageURL = "",
+    wgImageURL = ""
+)){
+
+    override fun initializeClusterEnvironment() {
         TODO("Not yet implemented")
     }
 
@@ -9,7 +24,11 @@ class TestBenchmark: Benchmark(config = emptyMap()) {
         TODO("Not yet implemented")
     }
 
-    override fun startWorkloadGenerator(wg: String, dimValue: Int, ucId: String) {
+    override fun startSUT(resources: Resource) {
+        TODO("Not yet implemented")
+    }
+
+    override fun startWorkloadGenerator(load: LoadDimension) {
         TODO("Not yet implemented")
     }
 }
