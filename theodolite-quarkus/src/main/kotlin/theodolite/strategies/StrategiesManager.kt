0cdba1d13c6062bc7a9db5600cd673579f6a16d5
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-02-19 15:08:18 +0100

    clean up

---
+++ theodolite-quarkus/src/main/kotlin/theodolite/strategies/StrategiesManager.kt
@@ -0,0 +1,31 @@
+package theodolite.strategies
+
+import theodolite.execution.BenchmarkExecutor
+import theodolite.strategies.restriction.LowerBoundRestriction
+import theodolite.strategies.restriction.RestrictionStrategy
+import theodolite.strategies.searchstrategy.BinarySearch
+import theodolite.strategies.searchstrategy.LinearSearch
+import theodolite.strategies.searchstrategy.SearchStrategy
+import theodolite.util.Results
+import java.lang.IllegalArgumentException
+
+class StrategiesManager {
+
+    fun createSearchStrategy(executor: BenchmarkExecutor, searchStrategyString: String): SearchStrategy {
+        return when (searchStrategyString) {
+            "LinearSearch" -> LinearSearch(executor)
+            "BinarySearch" -> BinarySearch(executor)
+            else -> throw  IllegalArgumentException("Search Strategy $searchStrategyString not found")
+        }
+    }
+
+    fun createRestrictionStrategy(results: Results, restrictionStrings: List<String>): Set<RestrictionStrategy> {
+        return restrictionStrings
+            .map { restriction ->
+                when (restriction) {
+                    "LowerBound" -> LowerBoundRestriction(results)
+                    else -> throw  IllegalArgumentException("Restriction Strategy $restrictionStrings not found")
+                }
+            }.toSet()
+    }
+}
