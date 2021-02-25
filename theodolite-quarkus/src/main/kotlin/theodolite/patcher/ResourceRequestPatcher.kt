a7aa0363d4f7d5ddb6679e4bb0fe4b1f3c89d0c4
lorenz <stu203404@mail.uni-kiel.de>
2021-02-25 23:44:50 +0100

    Fix RequestPatcher

--- theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceRequestPatcher.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceRequestPatcher.kt
@@ -2,19 +2,32 @@ package theodolite.patcher
 
 import io.fabric8.kubernetes.api.model.KubernetesResource
 import io.fabric8.kubernetes.api.model.Quantity
+import io.fabric8.kubernetes.api.model.ResourceRequirements
 import io.fabric8.kubernetes.api.model.apps.Deployment
-import io.fabric8.kubernetes.api.model.apps.StatefulSet
 
-class ResourceRequestPatcher(private val k8sResource: KubernetesResource, private val container: String, private val variableName: String): AbstractPatcher(k8sResource, container, variableName) {
+class ResourceRequestPatcher(
+    private val k8sResource: KubernetesResource,
+    private val container: String,
+    private val variableName: String
+) : AbstractPatcher(k8sResource, container, variableName) {
 
     override fun <String> patch(value: String) {
         if (k8sResource is Deployment) {
             k8sResource.spec.template.spec.containers.filter { it.name == container }.forEach {
-                it.resources.requests.replace(variableName,Quantity(value as kotlin.String))
-            }
-        } else if (k8sResource is StatefulSet) {
-            k8sResource.spec.template.spec.containers.filter { it.name == container }.forEach {
-                it.resources.requests.replace(variableName, Quantity(value as kotlin.String))
+                try {
+                    if (it.resources.requests.isEmpty()) {
+                        it.resources.requests = mapOf(variableName to Quantity(value as kotlin.String))
+                    } else {
+                        val values = mutableMapOf<kotlin.String, Quantity>()
+                        it.resources.requests.forEach { entry -> values.put(entry.key, entry.value) }
+                        values[variableName] = Quantity(value as kotlin.String)
+                        it.resources.requests = values
+                    }
+                } catch (e: IllegalStateException) {
+                    val resource = ResourceRequirements()
+                    resource.requests = mapOf(variableName to Quantity(value as kotlin.String))
+                    it.resources = resource
+                }
             }
         }
     }
