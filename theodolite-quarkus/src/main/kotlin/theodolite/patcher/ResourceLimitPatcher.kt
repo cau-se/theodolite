693a4ba1ccc4735f00b2e4645a818d78562bf327
lorenz <stu203404@mail.uni-kiel.de>
2021-02-25 22:17:37 +0100

    Fix the aggregation-deployment correct + ResourceLimitPatcher

--- theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceLimitPatcher.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceLimitPatcher.kt
@@ -3,31 +3,35 @@ package theodolite.patcher
 import io.fabric8.kubernetes.api.model.KubernetesResource
 import io.fabric8.kubernetes.api.model.Quantity
 import io.fabric8.kubernetes.api.model.ResourceRequirements
-import io.fabric8.kubernetes.api.model.ResourceRequirementsBuilder
 import io.fabric8.kubernetes.api.model.apps.Deployment
-import io.fabric8.kubernetes.api.model.apps.StatefulSet
-import java.lang.IllegalStateException
 
-class ResourceLimitPatcher(private val k8sResource: KubernetesResource, private val container: String, private val variableName: String): AbstractPatcher(k8sResource, container, variableName) {
+class ResourceLimitPatcher(
+    private val k8sResource: KubernetesResource,
+    private val container: String,
+    private val variableName: String
+) : AbstractPatcher(k8sResource, container, variableName) {
 
     override fun <String> patch(value: String) {
 
         if (k8sResource is Deployment) {
             k8sResource.spec.template.spec.containers.filter { it.name == container }.forEach {
                 try {
+                    println("before: " + it.resources.limits.toString())
+                    println("$variableName to : $value")
                     if (it.resources.limits.isNullOrEmpty()) {
                         it.resources.limits = mapOf(variableName to Quantity(value as kotlin.String))
                     } else {
-                        val values =  it.resources.limits
-                        println(values)
-                        values[variableName] = Quantity(value as kotlin.String)
-                        it.resources.limits = values
+                        val values = Quantity(value as kotlin.String)
+                        it.resources.limits[variableName] = values
                     }
                 } catch (e: IllegalStateException) {
                     val resource = ResourceRequirements()
                     resource.limits = mapOf(variableName to Quantity(value as kotlin.String))
                     it.resources = resource
                 }
+
+                println("after " + it.resources.limits.toString())
+
             }
         }
     }
