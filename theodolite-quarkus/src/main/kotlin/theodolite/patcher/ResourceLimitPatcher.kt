59bffdeba0e45116a738337b20604fb3185dd823
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-03-08 10:29:06 +0100

    minor code enhancements

--- theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceLimitPatcher.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceLimitPatcher.kt
@@ -8,25 +8,24 @@ import io.fabric8.kubernetes.api.model.apps.Deployment
 class ResourceLimitPatcher(
     private val k8sResource: KubernetesResource,
     private val container: String,
-    private val variableName: String
-) : AbstractPatcher(k8sResource, container, variableName) {
+    private val limitedResource: String
+) : AbstractPatcher(k8sResource, container, limitedResource) {
 
     override fun <String> patch(value: String) {
-
         if (k8sResource is Deployment) {
             k8sResource.spec.template.spec.containers.filter { it.name == container }.forEach {
                 try {
                     if (it.resources.limits.isEmpty()) {
-                        it.resources.limits = mapOf(variableName to Quantity(value as kotlin.String))
+                        it.resources.limits = mapOf(limitedResource to Quantity(value as kotlin.String))
                     } else {
                         val values = mutableMapOf<kotlin.String, Quantity>()
-                        it.resources.limits.forEach { entry -> values.put(entry.key, entry.value) }
-                        values[variableName] = Quantity(value as kotlin.String)
+                        it.resources.limits.forEach { entry -> values[entry.key] = entry.value }
+                        values[limitedResource] = Quantity(value as kotlin.String)
                         it.resources.limits = values
                     }
                 } catch (e: IllegalStateException) {
                     val resource = ResourceRequirements()
-                    resource.limits = mapOf(variableName to Quantity(value as kotlin.String))
+                    resource.limits = mapOf(limitedResource to Quantity(value as kotlin.String))
                     it.resources = resource
                 }
             }
--- theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceRequestPatcher.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/patcher/ResourceRequestPatcher.kt
@@ -8,24 +8,24 @@ import io.fabric8.kubernetes.api.model.apps.Deployment
 class ResourceRequestPatcher(
     private val k8sResource: KubernetesResource,
     private val container: String,
-    private val variableName: String
-) : AbstractPatcher(k8sResource, container, variableName) {
+    private val requestedResource: String
+) : AbstractPatcher(k8sResource, container, requestedResource) {
 
     override fun <String> patch(value: String) {
         if (k8sResource is Deployment) {
             k8sResource.spec.template.spec.containers.filter { it.name == container }.forEach {
                 try {
                     if (it.resources.requests.isEmpty()) {
-                        it.resources.requests = mapOf(variableName to Quantity(value as kotlin.String))
+                        it.resources.requests = mapOf(requestedResource to Quantity(value as kotlin.String))
                     } else {
                         val values = mutableMapOf<kotlin.String, Quantity>()
-                        it.resources.requests.forEach { entry -> values.put(entry.key, entry.value) }
-                        values[variableName] = Quantity(value as kotlin.String)
+                        it.resources.requests.forEach { entry -> values[entry.key] = entry.value }
+                        values[requestedResource] = Quantity(value as kotlin.String)
                         it.resources.requests = values
                     }
                 } catch (e: IllegalStateException) {
                     val resource = ResourceRequirements()
-                    resource.requests = mapOf(variableName to Quantity(value as kotlin.String))
+                    resource.requests = mapOf(requestedResource to Quantity(value as kotlin.String))
                     it.resources = resource
                 }
             }
