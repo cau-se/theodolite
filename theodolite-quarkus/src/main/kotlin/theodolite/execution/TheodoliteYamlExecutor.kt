cdb8036bfaebbc48d98cc9cf7fb6e888eb153c1e
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-03-09 21:51:05 +0100

    Restructure: Create main methods inside the TheodliteYamlExecutor

--- theodolite-quarkus/src/main/kotlin/theodolite/execution/TheodoliteYamlExecutor.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/execution/TheodoliteYamlExecutor.kt
@@ -1,11 +1,18 @@
 package theodolite.execution
 
+import io.quarkus.runtime.annotations.QuarkusMain
+import mu.KotlinLogging
 import theodolite.benchmark.BenchmarkExecution
 import theodolite.benchmark.KubernetesBenchmark
 import theodolite.util.YamlParser
+private val logger = KotlinLogging.logger {}
 
-class TheodoliteYamlExecutor {
-    fun run() {
+@QuarkusMain(name = "TheodoliteYamlExecutor")
+object TheodoliteYamlExecutor {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        logger.info { "Theodolite started" }
+
         // load the BenchmarkExecution and the BenchmarkType
         val parser = YamlParser()
         val benchmarkExecution =
@@ -15,5 +22,6 @@ class TheodoliteYamlExecutor {
 
         val executor = TheodoliteExecutor(benchmarkExecution, benchmark)
         executor.run()
+        logger.info { "Theodolite finished" }
     }
 }
