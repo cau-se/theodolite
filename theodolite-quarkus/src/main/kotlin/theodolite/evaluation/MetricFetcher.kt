2adc36125dafd470a9d30031161093748de622c0
lorenz <stu203404@mail.uni-kiel.de>
2021-03-11 18:32:48 +0100

    Add parsing and improve connection error handling

--- theodolite-quarkus/src/main/kotlin/theodolite/evaluation/MetricFetcher.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/evaluation/MetricFetcher.kt
@@ -1,13 +1,17 @@
 package theodolite.evaluation
 
+import com.google.gson.Gson
 import khttp.get
+import khttp.responses.Response
+import theodolite.util.PrometheusResponse
+import java.net.ConnectException
 import java.util.*
 
 
 class MetricFetcher(private val prometheusURL: String) {
 
-    fun fetchMetric(start: Long, end: Long, query: String): Any {
-
+    fun fetchMetric(start: Long, end: Long, query: String): PrometheusResponse {
+        var trys = 0
         val parameter = mapOf(
             "query" to query,
             "start" to toISODate(start),
@@ -15,19 +19,27 @@ class MetricFetcher(private val prometheusURL: String) {
             "step" to "5s"
         )
 
-        val response = get("$prometheusURL/api/v1/query_range", params = parameter)
-        //TODO FIX: NOTHING RECEIVED
-        return response.jsonObject.getJSONObject("data").getJSONArray("result")//.getJSONObject(0)["values"]
+        while (trys < 2) {
+            val response = get("$prometheusURL/api/v1/query_range", params = parameter)
+            if (response.statusCode != 200) {
+                trys++
+            } else {
+                return parseValues(response)
+            }
+        }
+        throw ConnectException("No answer from Prometheus received")
     }
 
     private fun toISODate(timestamp: Long): String {
         val sdf = java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'")
-        val date = Date(timestamp - (3600 * 1000))
+        val date = Date(timestamp - (3600 * 1000))//subtract 1h since cluster is in another timezone
         return sdf.format(date)
     }
 
-    private fun parseValues(values: String): Any {
-        // TODO("pars with gson")
-        return ""
+    private fun parseValues(values: Response): PrometheusResponse {
+        return Gson().fromJson<PrometheusResponse>(
+            values.jsonObject.toString(),
+            PrometheusResponse::class.java
+        )
     }
 }
