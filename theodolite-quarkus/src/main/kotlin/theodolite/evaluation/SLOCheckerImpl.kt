5891aa2ed0bfdec90ad2d86f32583c12010c37d6
lorenz <stu203404@mail.uni-kiel.de>
2021-03-11 18:32:48 +0100

    Add parsing and improve connection error handling

--- theodolite-quarkus/src/main/kotlin/theodolite/evaluation/SLOCheckerImpl.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/evaluation/SLOCheckerImpl.kt
@@ -2,18 +2,25 @@ package theodolite.evaluation
 
 import khttp.post
 import org.json.JSONObject
+import java.net.ConnectException
 
 class SLOCheckerImpl(private val prometheusURL: String) : SLOChecker {
 
     override fun evaluate(start: Long, end: Long): Boolean {
+        var counter = 0
         val metricFetcher = MetricFetcher(prometheusURL = prometheusURL)
-        val totalLag = metricFetcher.fetchMetric(start, end, "sum by(group)(kafka_consumergroup_group_lag > 0)")
-        val parameter = mapOf("total_lag" to totalLag)
+        val fetchedData = metricFetcher.fetchMetric(start, end, "sum by(group)(kafka_consumergroup_group_lag > 0)")
+        val data = JSONObject(mapOf("total_lag" to fetchedData.data?.result))
+        
+        while (counter < 2) {
+            val result = post("http://127.0.0.1:8000/evaluate-slope", data = data)
+            if (result.statusCode != 200) {
+                counter++
+            } else {
+                return result.text.toBoolean()
+            }
+        }
 
-        //val response = get("http://127.0.0.1:8000/evaluate-slope", params = parameter)
-        val post = post("http://127.0.0.1:8000/evaluate-slope", data = JSONObject(parameter))
-        //println(JSONObject(parameter))
-        //println(post.text.toBoolean())
-        return post.text.toBoolean()//response.jsonObject["suitable"] == "true"
+        throw ConnectException("Could not reach slope evaluation")
     }
 }
