595b2cf2ebffeecb75e9fa74b4f6f293892456f4
lorenz <stu203404@mail.uni-kiel.de>
2021-03-12 14:11:02 +0100

    Use Instant, Offset, exception on empty query and refactoring of python

--- slope-evaluator/api.py
+++ slope-evaluator/api.py
@@ -4,43 +4,41 @@ import logging
 import os
 import pandas as pd
 import json
-import numpy as np
-from fastapi.encoders import jsonable_encoder
+import sys
 
 app = FastAPI()
 
-def execute(results, threshold):
+logging.basicConfig(stream=sys.stdout,
+                    format="%(asctime)s %(levelname)s %(name)s: %(message)s")
+logger = logging.getLogger("Api")
 
+logger.setLevel(logging.INFO)
+
+def execute(results, threshold):
     d = []
     for result in results:
-        #print(results)
         group = result['metric']['group']
         for value in result['values']:
-            # print(value)
             d.append({'group': group, 'timestamp': int(
                 value[0]), 'value': int(value[1]) if value[1] != 'NaN' else 0})
 
     df = pd.DataFrame(d)
 
-    print(df)
+    logger.info(df)
     try:
         trend_slope = trend_slope_computer.compute(df, 0)
     except Exception as e:
         err_msg = 'Computing trend slope failed'
-        print(err_msg)
-        logging.exception(err_msg)
-        print('Mark this subexperiment as not successful and continue benchmark')
+        logger.exception(err_msg)
+        logger.error('Mark this subexperiment as not successful and continue benchmark')
         return False
 
-    print(f"Trend Slope: {trend_slope}")
+    logger.info("Trend Slope: %s", trend_slope)
 
     return trend_slope < threshold
 
 @app.post("/evaluate-slope",response_model=bool)
 async def evaluate_slope(request: Request):
-    print("request received")
-    x = json.loads(await request.body())
-    #x = np.array(x['total_lag'])
-    y = execute(x['total_lag'], 1000)
-    print(print(y))
-    return y
+    data = json.loads(await request.body())
+    logger.info("Request received")
+    return execute(data['total_lag'], data['threshold'])
--- theodolite-quarkus/src/main/kotlin/theodolite/evaluation/SLOCheckerImpl.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/evaluation/SLOCheckerImpl.kt
@@ -1,20 +1,22 @@
 package theodolite.evaluation
 
+import com.google.gson.Gson
 import khttp.post
-import org.json.JSONObject
 import java.net.ConnectException
+import java.time.Duration
+import java.time.Instant
 
-class SLOCheckerImpl(private val prometheusURL: String) : SLOChecker {
+class SLOCheckerImpl(private val prometheusURL: String, private val threshold: Int, private val offset: Duration) :
+    SLOChecker {
 
-    override fun evaluate(start: Long, end: Long): Boolean {
+    override fun evaluate(start: Instant, end: Instant): Boolean {
         var counter = 0
-        val metricFetcher = MetricFetcher(prometheusURL = prometheusURL)
+        val metricFetcher = MetricFetcher(prometheusURL = prometheusURL, offset = offset)
         val fetchedData = metricFetcher.fetchMetric(start, end, "sum by(group)(kafka_consumergroup_group_lag >= 0)")
-        val data = JSONObject(mapOf("total_lag" to fetchedData.data?.result))
-        
+        val data = Gson().toJson(mapOf("total_lag" to fetchedData.data?.result, "threshold" to threshold))
+
         while (counter < 2) {
-            // TODO handle timeouts
-            val result = post("http://127.0.0.1:8000/evaluate-slope", data = data)
+            val result = post("http://127.0.0.1:8000/evaluate-slope", data = data, timeout = 60.0)
             if (result.statusCode != 200) {
                 counter++
             } else {
