a733333f6ff40b83ac365a1d3f2b09543cfa69a3
lorenz <stu203404@mail.uni-kiel.de>
2021-03-23 20:05:40 +0100

    Add Reflection to PromResponse

--- theodolite-quarkus/src/main/kotlin/theodolite/util/PrometheusResponse.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/util/PrometheusResponse.kt
@@ -1,20 +1,26 @@
 package theodolite.util
 
+import io.quarkus.runtime.annotations.RegisterForReflection
+
+@RegisterForReflection
 data class PrometheusResponse(
     var status: String? = null,
     var data: PromData? = null
 )
 
+@RegisterForReflection
 data class PromData(
     var resultType: String? = null,
     var result: List<PromResult>? = null
 )
 
+@RegisterForReflection
 data class PromResult(
     var metric: PromMetric? = null,
     var values: List<Any>? = null
 )
 
+@RegisterForReflection
 data class PromMetric(
     var group: String? = null
 )
