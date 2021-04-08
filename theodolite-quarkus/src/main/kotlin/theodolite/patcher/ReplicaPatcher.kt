c9b9f892d1188a298c94938403c5678538cf4a80
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-04-08 21:07:21 +0200

    Fix ReplicaPatcher
    switch from Int to String and get the value to patch via parseInt(value)

--- theodolite-quarkus/src/main/kotlin/theodolite/patcher/ReplicaPatcher.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/patcher/ReplicaPatcher.kt
@@ -4,10 +4,10 @@ import io.fabric8.kubernetes.api.model.KubernetesResource
 import io.fabric8.kubernetes.api.model.apps.Deployment
 
 class ReplicaPatcher(private val k8sResource: KubernetesResource) : AbstractPatcher(k8sResource) {
-    override fun <Int> patch(value: Int) {
+    override fun <String> patch(value: String) {
         if (k8sResource is Deployment) {
-            if (value is kotlin.Int) {
-                this.k8sResource.spec.replicas = value
+            if (value is kotlin.String) {
+                this.k8sResource.spec.replicas = Integer.parseInt(value)
             }
         }
     }
