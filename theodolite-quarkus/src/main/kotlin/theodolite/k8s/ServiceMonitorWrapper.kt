c76b3e4ba4843a95b2a7b099e423cc3466bb66e9
lorenz <stu203404@mail.uni-kiel.de>
2021-04-13 15:29:18 +0200

    Fix ServiceMonitorWrapper.kt,
    Also fix to correct usage of tread{} on the ShutdownHook

--- theodolite-quarkus/src/main/kotlin/theodolite/k8s/ServiceMonitorWrapper.kt
+++ theodolite-quarkus/src/main/kotlin/theodolite/k8s/ServiceMonitorWrapper.kt
@@ -1,22 +1,27 @@
 package theodolite.k8s
 
-import io.fabric8.kubernetes.api.model.KubernetesResource
 import io.fabric8.kubernetes.client.CustomResource
 import io.fabric8.kubernetes.client.NamespacedKubernetesClient
 import mu.KotlinLogging
-import java.lang.Exception
 
 private val logger = KotlinLogging.logger {}
 
-class ServiceMonitorWrapper(private val serviceMonitor: Map<String,String>): CustomResource() {
+class ServiceMonitorWrapper(private val serviceMonitor: Map<String, String>) : CustomResource() {
 
     /**
      * Deploy a service monitor
      *
      * @param client a namespaced Kubernetes client which are used to deploy the CR object.
+     *
+     * @throws java.io.IOException if the resource could not be deployed.
      */
     fun deploy(client: NamespacedKubernetesClient) {
-        val serviceMonitorContext = K8sContextFactory().create(api = "v1", scope = "Namespaced", group = "monitoring.coreos.com", plural = "servicemonitors")
+        val serviceMonitorContext = K8sContextFactory().create(
+            api = "v1",
+            scope = "Namespaced",
+            group = "monitoring.coreos.com",
+            plural = "servicemonitors"
+        )
         client.customResource(serviceMonitorContext)
             .createOrReplace(client.configuration.namespace, this.serviceMonitor as Map<String, Any>)
     }
@@ -27,16 +32,25 @@ class ServiceMonitorWrapper(private val serviceMonitor: Map<String,String>): Cus
      * @param client a namespaced Kubernetes client which are used to delete the CR object.
      */
     fun delete(client: NamespacedKubernetesClient) {
-        val serviceMonitorContext = K8sContextFactory().create(api = "v1", scope = "Namespaced", group = "monitoring.coreos.com", plural = "servicemonitors")
+        val serviceMonitorContext = K8sContextFactory().create(
+            api = "v1",
+            scope = "Namespaced",
+            group = "monitoring.coreos.com",
+            plural = "servicemonitors"
+        )
         try {
-            client.customResource(serviceMonitorContext).delete(client.configuration.namespace, this.getServiceMonitorName())
+            client.customResource(serviceMonitorContext)
+                .delete(client.configuration.namespace, this.getServiceMonitorName())
         } catch (e: Exception) {
             logger.warn { "Could not delete service monitor" }
         }
     }
 
+    /**
+     * @throws NullPointerException if name or metadata is null
+     */
     private fun getServiceMonitorName(): String {
-        val smAsMap = this.serviceMonitor as Map<String, String>
-        return smAsMap["name"] !!
+        val smAsMap = this.serviceMonitor["metadata"]!! as Map<String, String>
+        return smAsMap["name"]!!
     }
 }
