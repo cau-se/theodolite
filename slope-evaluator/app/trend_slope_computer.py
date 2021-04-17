29f0ea17320ff6a9d9880c758aa45034270d636f
Sören Henning <soeren.henning@email.uni-kiel.de>
2021-04-17 16:39:33 +0200

    Correct column indices in lag trend computation

--- slope-evaluator/app/trend_slope_computer.py
+++ slope-evaluator/app/trend_slope_computer.py
@@ -4,15 +4,10 @@ import os
 
 def compute(data, warmup_sec):
     data['sec_start'] = data.loc[0:, 'timestamp'] - data.iloc[0]['timestamp']
-    print(data)
     regress = data.loc[data['sec_start'] >= warmup_sec] # Warm-Up
 
-    print(regress)
-
-    X = regress.iloc[:, 2].values.reshape(-1, 1)  # values converts it into a numpy array
-    print(X)
-    Y = regress.iloc[:, 3].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
-    print(Y)
+    X = regress.iloc[:, 1].values.reshape(-1, 1)  # values converts it into a numpy array
+    Y = regress.iloc[:, 2].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
     linear_regressor = LinearRegression()  # create object for the class
     linear_regressor.fit(X, Y)  # perform linear regression
     Y_pred = linear_regressor.predict(X)  # make predictions
