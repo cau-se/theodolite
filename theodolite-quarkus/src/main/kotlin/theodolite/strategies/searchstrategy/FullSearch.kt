34bde357951b6d1ee3b99446161c5abff4e049a9
Sören Henning <soeren.henning@email.uni-kiel.de>
2021-04-17 14:17:30 +0200

    Add FullSearch strategy

---
+++ theodolite-quarkus/src/main/kotlin/theodolite/strategies/searchstrategy/FullSearch.kt
@@ -0,0 +1,25 @@
+package theodolite.strategies.searchstrategy
+
+import mu.KotlinLogging
+import theodolite.execution.BenchmarkExecutor
+import theodolite.util.LoadDimension
+import theodolite.util.Resource
+
+private val logger = KotlinLogging.logger {}
+
+/**
+ *  Linear-search-like implementation for determining the smallest suitable number of instances.
+ *
+ * @param benchmarkExecutor Benchmark executor which runs the individual benchmarks.
+ */
+class LinearSearch(benchmarkExecutor: BenchmarkExecutor) : SearchStrategy(benchmarkExecutor) {
+
+    override fun findSuitableResource(load: LoadDimension, resources: List<Resource>): Resource? {
+        for (res in resources) {
+
+            logger.info { "Running experiment with load '$load' and resources '$res'" }
+            if (this.benchmarkExecutor.runExperiment(load, res)) return res
+        }
+        return null
+    }
+}
