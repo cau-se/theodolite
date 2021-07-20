aca4ca4830467a3cbef0a7b630ecd00908c1e090
Sören Henning <soeren.henning@email.uni-kiel.de>
2021-07-20 17:32:37 +0200

    Change directory structure of benchmark defs

--- theodolite-benchmarks/definitions/install-configmaps.sh
+++ theodolite-benchmarks/definitions/install-configmaps.sh
@@ -1,17 +1,8 @@
-cd uc1-flink/
-kubectl create configmap benchmark-resources-uc1-flink --from-file .
-cd ../uc2-flink/
-kubectl create configmap benchmark-resources-uc2-flink --from-file .
-cd ../uc3-flink/
-kubectl create configmap benchmark-resources-uc3-flink --from-file .
-cd ../uc4-flink/
-kubectl create configmap benchmark-resources-uc4-flink --from-file .
-cd ../uc1-kstreams/
-kubectl create configmap benchmark-resources-uc1-kstreams --from-file .
-cd ../uc2-kstreams/
-kubectl create configmap benchmark-resources-uc2-kstreams --from-file .
-cd ../uc3-kstreams/
-kubectl create configmap benchmark-resources-uc3-kstreams --from-file .
-cd ../uc4-kstreams/
-kubectl create configmap benchmark-resources-uc4-kstreams --from-file .
-cd ..
+kubectl create configmap benchmark-resources-uc1-flink --from-file uc1-flink/resources
+kubectl create configmap benchmark-resources-uc2-flink --from-file uc2-flink/resources
+kubectl create configmap benchmark-resources-uc3-flink --from-file uc3-flink/resources
+kubectl create configmap benchmark-resources-uc4-flink --from-file uc4-flink/resources
+kubectl create configmap benchmark-resources-uc1-kstreams --from-file uc1-kstreams/resources
+kubectl create configmap benchmark-resources-uc2-kstreams --from-file uc2-kstreams/resources
+kubectl create configmap benchmark-resources-uc3-kstreams --from-file uc3-kstreams/resources
+kubectl create configmap benchmark-resources-uc4-kstreams --from-file uc4-kstreams/resources
