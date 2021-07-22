4a1ddfd7b301160a384c2d137795bdad2e77c752
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-07-22 16:36:20 +0200

    change default values

--- theodolite/src/main/kotlin/theodolite/execution/TheodoliteStandalone.kt
+++ theodolite/src/main/kotlin/theodolite/execution/TheodoliteStandalone.kt
@@ -31,8 +31,8 @@ class TheodoliteStandalone {
     fun start() {
         logger.info { "Theodolite started" }
 
-        val executionPath = System.getenv("THEODOLITE_EXECUTION") ?: "./examples/standalone/example-execution.yaml"
-        val benchmarkPath = System.getenv("THEODOLITE_BENCHMARK") ?: "./examples/standalone/example-benchmark.yaml"
+        val executionPath = System.getenv("THEODOLITE_EXECUTION") ?: "execution/execution.yaml"
+        val benchmarkPath = System.getenv("THEODOLITE_BENCHMARK") ?: "benchmark/benchmark.yaml"
 
         logger.info { "Using $executionPath for BenchmarkExecution" }
         logger.info { "Using $benchmarkPath for BenchmarkType" }
