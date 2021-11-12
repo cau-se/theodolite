c00b463409eacdb732b80daf134a7e5e660fae61
lorenz <stu203404@mail.uni-kiel.de>
2021-11-12 15:55:28 +0100

    Cleanup checkstyle of uc3-beam-flink

--- theodolite-benchmarks/uc3-beam-flink/src/main/java/application/MapTimeFormat.java
+++ theodolite-benchmarks/uc3-beam-flink/src/main/java/application/MapTimeFormat.java
@@ -1,25 +1,27 @@
 package application;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.values.KV;
 import titan.ccp.model.records.ActivePowerRecord;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-
+/**
+ * Changes the time format to us europe/paris time.
+ */
 public class MapTimeFormat extends SimpleFunction<KV<String, ActivePowerRecord>, KV<HourOfDayKey,
     ActivePowerRecord>> {
-    private final ZoneId zone = ZoneId.of("Europe/Paris");
-  final StatsKeyFactory<HourOfDayKey> keyFactory = new HourOfDayKeyFactory();
+  private static final long serialVersionUID = -6597391279968647035L;
+  private final StatsKeyFactory<HourOfDayKey> keyFactory = new HourOfDayKeyFactory();
+  private final ZoneId zone = ZoneId.of("Europe/Paris");
 
-    @Override
-    public KV<application.HourOfDayKey, ActivePowerRecord> apply(
-    final KV<String, ActivePowerRecord> kv) {
-      final Instant instant = Instant.ofEpochMilli(kv.getValue().getTimestamp());
-      final LocalDateTime dateTime = LocalDateTime.ofInstant(instant, this.zone);
-      return KV.of(keyFactory.createKey(kv.getValue().getIdentifier(), dateTime),
-          kv.getValue());
-    }
+  @Override
+  public KV<application.HourOfDayKey, ActivePowerRecord> apply(
+      final KV<String, ActivePowerRecord> kv) {
+    final Instant instant = Instant.ofEpochMilli(kv.getValue().getTimestamp());
+    final LocalDateTime dateTime = LocalDateTime.ofInstant(instant, this.zone);
+    return KV.of(keyFactory.createKey(kv.getValue().getIdentifier(), dateTime),
+        kv.getValue());
   }
 }
