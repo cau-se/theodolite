57ba13e7c49fdc3c7823ccd5a6987b167b2a2c3e
lorenz <stu203404@mail.uni-kiel.de>
2021-11-19 11:17:07 +0100

    Add uc2-beam-samza

--- theodolite-benchmarks/uc2-beam-flink/src/main/java/application/Uc2BeamFlink.java
+++ theodolite-benchmarks/uc2-beam-flink/src/main/java/application/Uc2BeamFlink.java
@@ -11,15 +11,6 @@ import theodolite.commons.beam.AbstractBeamService;
  * using--flinkMaster as run parameter.
  */
 public final class Uc2BeamFlink extends AbstractBeamService {
-  private static final String JOB_NAME = "Uc2Application";
-  private static final String BOOTSTRAP = "KAFKA_BOOTSTRAP_SERVERS";
-  private static final String INPUT = "INPUT";
-  private static final String OUTPUT = "OUTPUT";
-  private static final String SCHEMA_REGISTRY = "SCHEMA_REGISTRY_URL";
-  private static final String YES = "true";
-  private static final String USE_AVRO_READER = YES;
-  private static final String AUTO_COMMIT_CONFIG = YES;
-  private static final String KAFKA_WINDOW_DURATION_MINUTES = "KAFKA_WINDOW_DURATION_MINUTES";
 
   /**
    * Private constructor setting specific options for this use case.
@@ -35,9 +26,9 @@ public final class Uc2BeamFlink extends AbstractBeamService {
   @SuppressWarnings({"serial", "unchecked", "rawtypes"})
   public static void main(final String[] args) {
 
-    Uc2BeamFlink uc2BeamFlink = new Uc2BeamFlink(args);
+    final Uc2BeamFlink uc2BeamFlink = new Uc2BeamFlink(args);
 
-    Pipeline pipeline = new Uc2BeamPipeline(uc2BeamFlink.options, uc2BeamFlink.getConfig());
+    final Pipeline pipeline = new Uc2BeamPipeline(uc2BeamFlink.options, uc2BeamFlink.getConfig());
 
     pipeline.run().waitUntilFinish();
   }
