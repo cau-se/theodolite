96f18bb4f7e2ee3e1c955bd5e71f56fedb62f3f0
Sören Henning <soeren.henning@email.uni-kiel.de>
2022-01-22 23:48:51 +0100

    Log stack trace when execution failed

--- theodolite/src/main/kotlin/theodolite/execution/operator/TheodoliteController.kt
+++ theodolite/src/main/kotlin/theodolite/execution/operator/TheodoliteController.kt
@@ -110,8 +110,7 @@ class TheodoliteController(
                 type = "WARNING",
                 reason = "Execution failed",
                 message = "An error occurs while executing:  ${e.message}")
-            logger.error { "Failure while executing execution ${execution.name} with benchmark ${benchmark.name}." }
-            logger.error { "Problem is: $e" }
+            logger.error(e) { "Failure while executing execution ${execution.name} with benchmark ${benchmark.name}." }
             executionStateHandler.setExecutionState(execution.name, ExecutionStates.FAILURE)
         }
         executionStateHandler.stopDurationStateTimer()
