c7cce0eccc26bd852d061d0d004ac9a50062f1af
Sören Henning <soeren.henning@email.uni-kiel.de>
2022-01-28 13:01:49 +0100

    Pull quietly

--- theodolite-benchmarks/docker-test/smoketest-runner.sh
+++ theodolite-benchmarks/docker-test/smoketest-runner.sh
@@ -4,7 +4,7 @@ COMPOSE_FILE_PATH=$1
 echo "Run test for '$COMPOSE_FILE_PATH'."
 
 cd $COMPOSE_FILE_PATH
-docker-compose pull
+docker-compose pull -q
 docker-compose up -d kafka zookeeper schema-registry
 sleep 10s
 docker-compose up -d
