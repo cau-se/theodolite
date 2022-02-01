b1ffea352a8023d11000ab78d212220f43924129
Sören Henning <soeren.henning@email.uni-kiel.de>
2022-02-01 19:20:16 +0100

    Fix TTY for UC3 tests

--- theodolite-benchmarks/docker-test/uc3-kstreams/test.sh
+++ theodolite-benchmarks/docker-test/uc3-kstreams/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose exec kcat kcat -C -b kafka:9092 -t output -s key=s -s value=s -r http://schema-registry:8081 -f '%k:%s\n' -c 600 |
+docker-compose exec -T kcat kcat -C -b kafka:9092 -t output -s key=s -s value=s -r http://schema-registry:8081 -f '%k:%s\n' -c 600 |
     tee /dev/stderr |
     awk -F ':' '!/^%/ {print $1}' |
     sort |
