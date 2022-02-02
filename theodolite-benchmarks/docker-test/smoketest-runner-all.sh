7388b81e588e6a374345294e861b5462976acc32
Sören Henning <soeren.henning@email.uni-kiel.de>
2022-02-02 10:42:04 +0100

    Minor format fixes for smoketest all runner

--- theodolite-benchmarks/docker-test/smoketest-runner-all.sh
+++ theodolite-benchmarks/docker-test/smoketest-runner-all.sh
@@ -4,4 +4,4 @@ find . -name 'test.sh' -type f -exec dirname {} \; |
     sort |
     xargs -I %s sh -c "./smoketest-runner.sh %s 1>&2; echo $?" |
     sort |
-    awk '{count[$1!=0]++} END {print count[0] " tests successful, " count[1] " test failed."}; exit count[1]'
+    awk 'BEGIN {count[0]=0; count[1]=0} {count[$1!=0]++} END {print count[0] " tests successful, " count[1] " test failed."; exit count[1]}'
