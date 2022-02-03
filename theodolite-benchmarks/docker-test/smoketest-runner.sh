331be72ad269c95ab4498b4e0ec6e30bc8c3a056
Sören Henning <soeren.henning@email.uni-kiel.de>
2022-02-03 18:09:55 +0100

    Increase sleep for topic creationg

--- theodolite-benchmarks/docker-test/smoketest-runner.sh
+++ theodolite-benchmarks/docker-test/smoketest-runner.sh
@@ -6,7 +6,7 @@ echo "Run test for '$COMPOSE_FILE_PATH'."
 cd $COMPOSE_FILE_PATH
 docker-compose pull -q
 docker-compose up -d kafka zookeeper schema-registry
-sleep 20s
+sleep 30s
 docker-compose up -d
 sleep 5s
 docker-compose ps
