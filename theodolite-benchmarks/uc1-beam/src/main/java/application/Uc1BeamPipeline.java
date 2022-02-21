44c160ff4574a099a0f763c1476fa9e87e528453
Sören Henning <soeren.henning@email.uni-kiel.de>
2022-02-21 17:55:07 +0100

    Add basic Pub/Sub source

--- theodolite-benchmarks/uc1-beam/src/main/java/application/Uc1BeamPipeline.java
+++ theodolite-benchmarks/uc1-beam/src/main/java/application/Uc1BeamPipeline.java
@@ -1,9 +1,15 @@
 package application;
 
+import application.pubsub.PubSubEncoder;
 import org.apache.beam.sdk.coders.AvroCoder;
 import org.apache.beam.sdk.coders.CoderRegistry;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO.Read;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.PCollection;
 import org.apache.commons.configuration2.Configuration;
 import theodolite.commons.beam.AbstractPipeline;
 import theodolite.commons.beam.kafka.KafkaActivePowerTimestampReader;
@@ -15,24 +21,40 @@ import titan.ccp.model.records.ActivePowerRecord;
 public final class Uc1BeamPipeline extends AbstractPipeline {
 
   public static final String SINK_TYPE_KEY = "sink.type";
+  public static final String SOURCE_TYPE_KEY = "source.type";
 
   protected Uc1BeamPipeline(final PipelineOptions options, final Configuration config) {
     super(options, config);
 
-    final SinkType sinkType = SinkType.from(config.getString(SINK_TYPE_KEY));
-
     // Set Coders for classes that will be distributed
     final CoderRegistry cr = super.getCoderRegistry();
-    cr.registerCoderForClass(ActivePowerRecord.class, AvroCoder.of(ActivePowerRecord.SCHEMA$));
+    cr.registerCoderForClass(ActivePowerRecord.class,
+        AvroCoder.of(ActivePowerRecord.class, false));
 
-    final KafkaActivePowerTimestampReader kafka = new KafkaActivePowerTimestampReader(
-        super.bootstrapServer,
-        super.inputTopic,
-        super.buildConsumerConfig());
+    final SinkType sinkType = SinkType.from(config.getString(SINK_TYPE_KEY));
 
-    super.apply(kafka)
-        .apply(Values.create())
-        .apply(sinkType.create(config));
+    final String sourceType = config.getString(SOURCE_TYPE_KEY);
+
+    PCollection<ActivePowerRecord> activePowerRecords;
+
+    if (sourceType == "pubsub") {
+      final String topic = "input";
+      final String project = "dummy-project-id";
+      final Read<PubsubMessage> pubsub = PubsubIO
+          .readMessages()
+          .fromTopic("projects/" + project + "/topics/" + topic);
+
+      activePowerRecords = super.apply(pubsub).apply(MapElements.via(new PubSubEncoder()));
+    } else {
+      final KafkaActivePowerTimestampReader kafka = new KafkaActivePowerTimestampReader(
+          super.bootstrapServer,
+          super.inputTopic,
+          super.buildConsumerConfig());
+
+      activePowerRecords = super.apply(kafka).apply(Values.create());
+    }
+
+    activePowerRecords.apply(sinkType.create(config));
   }
 
 }
