7ec4f98490702c0401b320267d30a910c7fb4065
Sören Henning <soeren.henning@email.uni-kiel.de>
2022-03-09 15:20:18 +0100

    Add debug logs to Firestore sink

--- theodolite-benchmarks/uc1-beam/src/main/java/rocks/theodolite/benchmarks/uc1/beam/firestore/FirestoreSink.java
+++ theodolite-benchmarks/uc1-beam/src/main/java/rocks/theodolite/benchmarks/uc1/beam/firestore/FirestoreSink.java
@@ -2,11 +2,19 @@ package rocks.theodolite.benchmarks.uc1.beam.firestore;
 
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.firestore.v1.Document;
+import com.google.firestore.v1.Value;
+import com.google.firestore.v1.Write;
+import java.util.Map;
 import org.apache.beam.sdk.io.gcp.firestore.FirestoreIO;
+import org.apache.beam.sdk.io.gcp.firestore.FirestoreV1.WriteSuccessSummary;
+import org.apache.beam.sdk.io.gcp.firestore.RpcQosOptions;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.commons.configuration2.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import titan.ccp.model.records.ActivePowerRecord;
 
 /**
@@ -17,13 +25,26 @@ public class FirestoreSink extends PTransform<PCollection<ActivePowerRecord>, PC
 
   public static final String SINK_FIRESTORE_COLLECTION_KEY = "sink.firestore.collection";
 
+  public static final String SINK_FIRESTORE_MAX_WORKERS_KEY = "sink.firestore.hint.max.workers";
+
   private static final long serialVersionUID = 1L;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FirestoreSink.class);
 
   private final String collectionName;
 
-  public FirestoreSink(final String collectionName) {
+  private final RpcQosOptions rpcQosOptions;
+
+  /**
+   * Create a new {@link FirestoreSink} based on the provided collection name and hint for the max.
+   * number of workers.
+   */
+  public FirestoreSink(final String collectionName, final int hintMaxNumWorkers) {
     super();
     this.collectionName = collectionName;
+    this.rpcQosOptions = RpcQosOptions.newBuilder()
+        .withHintMaxNumWorkers(hintMaxNumWorkers)
+        .build();
   }
 
   @Override
@@ -31,11 +52,42 @@ public class FirestoreSink extends PTransform<PCollection<ActivePowerRecord>, PC
     return activePowerRecords
         .apply(MapElements.via(new DocumentMapper(this.collectionName)))
         .apply(MapElements.via(new UpdateOperationMapper()))
-        .apply(FirestoreIO.v1().write().batchWrite().build());
+        .apply(MapElements.via(
+            new SimpleFunction<Write, Write>() {
+
+              private static final long serialVersionUID = 1L;
+
+              @Override
+              public Write apply(final Write write) {
+                final long time = System.currentTimeMillis();
+                final Map<String, Value> map = write.getUpdate().getFieldsMap();
+                LOGGER.info("[{}] Initiate write record {} {}", time,
+                    map.get("identifier").getStringValue(),
+                    map.get("timestamp").getIntegerValue());
+                return write;
+              }
+            }))
+        .apply(FirestoreIO.v1().write().batchWrite().withRpcQosOptions(this.rpcQosOptions).build())
+        .apply(MapElements.via(
+            new SimpleFunction<WriteSuccessSummary, String>() {
+
+              private static final long serialVersionUID = 1L;
+
+              @Override
+              public String apply(final WriteSuccessSummary summary) {
+                final long time = System.currentTimeMillis();
+                LOGGER.info("[{}] Finished write with result {}", time, summary.toString());
+                return summary.toString();
+              }
+            }));
   }
 
+  /**
+   * Create a {@link FirestoreSink} from the provided {@link Configuration} object.
+   */
   public static FirestoreSink fromConfig(final Configuration config) {
-    final String collectionName = config.getString(SINK_FIRESTORE_COLLECTION_KEY);
-    return new FirestoreSink(collectionName);
+    final String collectionName = config.getString(SINK_FIRESTORE_MAX_WORKERS_KEY);
+    final int maxWorkers = config.getInt(SINK_FIRESTORE_MAX_WORKERS_KEY, 500);
+    return new FirestoreSink(collectionName, maxWorkers);
   }
 }
