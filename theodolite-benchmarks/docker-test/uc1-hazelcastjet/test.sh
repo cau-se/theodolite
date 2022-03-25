76170ebce945b15cfb4aa65d70d4dbed87cbfad5
lorenz <stu203404@mail.uni-kiel.de>
2022-03-25 23:25:58 +0100

    Adapt smoke test uc1 to the new logging method

--- theodolite-benchmarks/docker-test/uc1-hazelcastjet/test.sh
+++ theodolite-benchmarks/docker-test/uc1-hazelcastjet/test.sh
@@ -3,8 +3,8 @@
 sleep 55s # to let the benchmark and produce some output
 
 docker-compose logs --tail 100 benchmark |
-    sed -n 's/^.*key":"//p' | # cut the first part before the key
-    sed 's/","value.*//' | # cut the rest after the key
+    sed -n 's/^.*identifier":"//p' | # cut the first part before the key
+    sed 's/","timestamp.*//' | # cut the rest after the key
     sort |
     uniq |
     wc -l |
