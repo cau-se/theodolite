d77583e7cd2f73aa34167b899abb52dcaef8d16a
lorenz <stu203404@mail.uni-kiel.de>
2022-05-20 15:40:28 +0200

    Extend use case hazelcast ConfigurationKeys

--- theodolite-benchmarks/hazelcastjet-commons/src/main/java/rocks/theodolite/benchmarks/commons/hazelcastjet/ConfigurationKeys.java
+++ theodolite-benchmarks/hazelcastjet-commons/src/main/java/rocks/theodolite/benchmarks/commons/hazelcastjet/ConfigurationKeys.java
@@ -5,15 +5,17 @@ package rocks.theodolite.benchmarks.commons.hazelcastjet;
  */
 public class ConfigurationKeys {
 
+  public static final String APPLICATION_NAME = "application.name";
+
   // Common Keys
   public static final String BOOTSTRAP_SERVER = "BOOTSTRAP_SERVER";
   public static final String KUBERNETES_DNS_NAME = "KUBERNETES_DNS_NAME";
   public static final String PORT = "PORT";
   public static final String PORT_AUTO_INCREMENT = "PORT_AUTO_INCREMENT";
   public static final String CLUSTER_NAME_PREFIX = "CLUSTER_NAME_PREFIX";
-  public static final String KAFKA_BOOTSTRAP_SERVERS = "KAFKA_BOOTSTRAP_SERVERS";
-  public static final String SCHEMA_REGISTRY_URL = "SCHEMA_REGISTRY_URL";
-  public static final String KAFKA_INPUT_TOPIC = "KAFKA_INPUT_TOPIC";
+  public static final String KAFKA_BOOTSTRAP_SERVERS = "kafka.bootstrap.servers";
+  public static final String SCHEMA_REGISTRY_URL = "schema.registry.url";
+  public static final String KAFKA_INPUT_TOPIC = "kafka.input.topic";
 
   // Additional topics
   public static final String KAFKA_OUTPUT_TOPIC = "KAFKA_OUTPUT_TOPIC";
