cau-se/theodolite

commit f19a148c0bb7b216e1b27d1956e6c089f0cb5a6a

Fix issue in restriction search, fix #402

theodolite/src/main/kotlin/rocks/theodolite/core/strategies/restrictionstrategy/LowerBoundRestriction.kt
@@ -13,11 +13,9 @@ import rocks.theodolite.core.Results
 class LowerBoundRestriction(results: Results) : RestrictionStrategy(results) {
 
     override fun apply(xValue: Int, yValues: List<Int>): List<Int> {
-        val maxXValue: Int? = this.results.getMaxBenchmarkedXDimensionValue(xValue)
-        var lowerBound: Int? = this.results.getOptYDimensionValue(maxXValue)
-        if (lowerBound == null) {
-            lowerBound = yValues[0]
-        }
+        val maxXValue = this.results.getMaxBenchmarkedXDimensionValue(xValue)
+        // Get previous largest y value or restrict to empty list
+        val lowerBound: Int = this.results.getOptYDimensionValue(maxXValue) ?: return listOf()
         return yValues.filter { x -> x >= lowerBound }
     }
 

theodolite/src/test/kotlin/rocks/theodolite/core/strategies/searchstrategy/InitialGuessSearchStrategyTest.kt
@@ -9,11 +9,36 @@ import rocks.theodolite.kubernetes.TestExperimentRunner
 import rocks.theodolite.core.strategies.guessstrategy.PrevInstanceOptGuess
 import rocks.theodolite.core.Results
 import rocks.theodolite.core.createResultsFromArray
+import rocks.theodolite.core.strategies.restrictionstrategy.LowerBoundRestriction
 
 private val logger = KotlinLogging.logger {}
 
 @QuarkusTest
 class InitialGuessSearchStrategyTest {
+
+    @Test
+    fun initialGuessSearchNoMatch() {
+        val mockResults = createResultsFromArray(arrayOf(
+            arrayOf(true, true),
+            arrayOf(false, false),
+            arrayOf(true, true),
+        ), Metric.DEMAND)
+        val mockLoads: List<Int> = (1..3).toList()
+        val mockResources: List<Int> = (1..2).toList()
+        val results = Results(Metric.DEMAND)
+        val guessStrategy = PrevInstanceOptGuess()
+        val benchmarkExecutor = TestExperimentRunner(results, mockResults)
+        val strategy = InitialGuessSearchStrategy(benchmarkExecutor,guessStrategy, results)
+
+        val actual: MutableList<Int?> = mutableListOf()
+        val expected: List<Int?> = listOf(1, null, 1)
+
+        for (load in mockLoads) {
+            actual.add(strategy.findSuitableResource(load, mockResources))
+        }
+
+        assertEquals(expected, actual)
+    }
 
     @Test
     fun testInitialGuessSearch() {

theodolite/src/test/kotlin/rocks/theodolite/core/strategies/searchstrategy/RestrictionSearchTest.kt
@@ -14,7 +14,6 @@ import rocks.theodolite.core.createResultsFromArray
 class RestrictionSearchTest {
 
     @Test
-    @Disabled("Currently failing, has to be fixed")
     fun restrictionSearchNoMatch() {
         val mockResults = createResultsFromArray(arrayOf(
             arrayOf(true, true),

commit c6b073a08ccd77658cd6ff8a8b0de16a93d11999

Fix bug introduced with f19a148c0bb7b216e1b27d1956e6c089f0cb5a6a

theodolite/src/main/kotlin/rocks/theodolite/core/strategies/restrictionstrategy/LowerBoundRestriction.kt
@@ -1,6 +1,7 @@
 package rocks.theodolite.core.strategies.restrictionstrategy
 
 import rocks.theodolite.core.Results
+import java.util.*
 
 /**
  * The [LowerBoundRestriction] sets the lower bound of the resources to be examined in the experiment to the value
@@ -13,7 +14,8 @@ import rocks.theodolite.core.Results
 class LowerBoundRestriction(results: Results) : RestrictionStrategy(results) {
 
     override fun apply(xValue: Int, yValues: List<Int>): List<Int> {
-        val maxXValue = this.results.getMaxBenchmarkedXDimensionValue(xValue)
+        // Get previous largest x value or return full list
+        val maxXValue = this.results.getMaxBenchmarkedXDimensionValue(xValue) ?: return yValues.toList()
         // Get previous largest y value or restrict to empty list
         val lowerBound: Int = this.results.getOptYDimensionValue(maxXValue) ?: return listOf()
         return yValues.filter { x -> x >= lowerBound }