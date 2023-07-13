e2cfcc4ea469bf0037e0beea010e235e41ee24fb
Sören Henning <soeren.henning@jku.at>
2023-07-13 15:14:56 +0200

    Remove dropped records SLO (fix #317)

--- theodolite/src/main/kotlin/rocks/theodolite/kubernetes/slo/SloTypes.kt
+++ theodolite/src/main/kotlin/rocks/theodolite/kubernetes/slo/SloTypes.kt
@@ -3,10 +3,7 @@ package rocks.theodolite.kubernetes.slo
 enum class SloTypes(val value: String) {
     GENERIC("generic"),
     LAG_TREND("lag trend"),
-    @Deprecated("Use LAG_TREND with relative threshold instead.") LAG_TREND_RATIO("lag trend ratio"),
-    @Deprecated("Use GENERIC instead.") DROPPED_RECORDS("dropped records"),
-    @Deprecated("Use GENERIC with relative threshold instead.") DROPPED_RECORDS_RATIO("dropped records ratio");
-
+    @Deprecated("Use LAG_TREND with relative threshold instead.") LAG_TREND_RATIO("lag trend ratio");
     companion object {
         fun from(type: String): SloTypes =
             values().find { it.value == type } ?: throw IllegalArgumentException("Requested SLO does not exist")
