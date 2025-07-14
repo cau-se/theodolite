1cb89e040bf04af1d39b905b41d815661a579acd
Sören Henning <soeren.henning@jku.at>
2025-07-14 14:27:17 +0200

    Deprecate standalone mode and make operator mode the new default

--- theodolite/src/main/kotlin/rocks/theodolite/kubernetes/Configuration.kt
+++ theodolite/src/main/kotlin/rocks/theodolite/kubernetes/Configuration.kt
@@ -9,7 +9,7 @@ class Configuration {
     companion object {
         val NAMESPACE = System.getenv("NAMESPACE") ?: DEFAULT_NAMESPACE
         val COMPONENT_NAME = System.getenv("COMPONENT_NAME") ?: DEFAULT_COMPONENT_NAME
-        val EXECUTION_MODE = System.getenv("MODE") ?: ExecutionModes.STANDALONE.value
+        val EXECUTION_MODE = System.getenv("MODE") ?: ExecutionModes.OPERATOR.value
 
         /**
          * Specifies how long Theodolite should wait (in sec) before aborting the execution of an action command.
